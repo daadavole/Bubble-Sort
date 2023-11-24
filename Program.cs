67524572a70722ae7dae6fe5c2a918ad613db773
daadavole <dostaldavid@post.cz>
2023-11-24 14:04:42 +0100

    Implemented Bubble sort

--- Program.cs
+++ Program.cs
@@ -8,14 +8,13 @@ namespace Bubble_Sort
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            List<int> list = new List<int>(); 
+            List<int> list = new List<int> { 2, 8, 5, 3, 9, 4, 1}; 
             Console.WriteLine("Starting list: ");
             PrintList(list);
-            Console.WriteLine("Sorting sequence: ");
-
-
+            Console.WriteLine("Sorted list: ");
+            PrintList(BubbleSort(list));
 
             Console.ReadKey();
         }
@@ -27,13 +26,31 @@ namespace Bubble_Sort
             }
             Console.WriteLine();
         }
-        static void BubbleSort(List<int> list)
+        static List<int> BubbleSort(List<int> list)
         {
-            bool swap = false;
-            for (int i = 0; i < list.Count; i++)
+            bool swap;
+            do
             {
+                swap = false;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (i + 1 < list.Count)
+                    {
+                        if (list[i] > list[i + 1])
+                        {
+                            // Swap values at index i and i + 1
+                            int holder = list[i + 1];
+                            list[i + 1] = list[i];
+                            list[i] = holder;
+                            swap = true;
+                            // Can be used for better visualisation
+                            //PrintList(list);
+                        }
+                    }
+                }
+            } while (swap);
 
-            }
+            return list;
         }
     }
 }
